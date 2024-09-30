4fcf4ae55a4e2ce85446f3d4cf0d602d222115bd
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-09-30 09:52:52 -0300

    first compile

---
+++ build/MS07/healthmonitor.d.ts
@@ -0,0 +1,7 @@
+export type cnt_heartbeat = {
+    id: string;
+    status: string;
+    timestamp: string;
+    url: string;
+    action: string;
+};
---
+++ build/index.d.ts
@@ -0,0 +1,1 @@
+export * from './MS07/healthmonitor.js';
