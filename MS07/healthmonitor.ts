b8435b59a7b5bd7b6c5fc907a686f1cd4a29c64a
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-09-30 14:56:55 -0300

    contrato

--- MS07/healthmonitor.ts
+++ MS07/healthmonitor.ts
@@ -1,7 +1,26 @@
-export type cnt_heartbeat = {
+export type sch_heartbeat = {
     id: string;
     status: string;
     timestamp: string;
     url: string;
     action: string;
+}
+
+export class cnt_heartbeat implements sch_heartbeat {
+    id: string="";
+    status: string="";
+    timestamp: string="";
+    url: string="";
+    action: string="";
+    static fromBody(body: any) {
+
+        const certificado: cnt_heartbeat = {
+            id: body.id || "",
+            status: body.status || "",
+            timestamp: body.timestamp || new Date().toDateString(),
+            url: body.url || "",
+            action: body.certificado || ""
+        };
+        return certificado;
+    }
 }
