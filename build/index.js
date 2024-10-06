993bc49e11cbf16d393e8c1d45a60971e76e97bd
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-10-05 23:47:32 -0300

    configbase2

--- build/index.js
+++ build/index.js
@@ -15,3 +15,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./MS07/healthmonitor.js"), exports);
+__exportStar(require("./MS08/database.js"), exports);
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
 export * from './MS07/healthmonitor.js';
+export * from './MS08/database.js';
